bb3aa9f4c9970fdcb4d0ac8aebcb45539e7c1e69
jyeshtha1799 <114448454+jyeshtha1799@users.noreply.github.com>
2024-08-06 17:56:02 -0400

    PromptTemplate for evaluating whether a given answer is useful in resolving a specific question

--- main.py
+++ main.py
@@ -146,3 +146,27 @@ for uploaded_file in uploaded_files:
     hallucination_grader.invoke({"documents": docs, "generation": generation})
 
     
+
+        ### Answer Grader 
+
+    # LLM
+    llm = ChatOllama(model=local_llm, format="json", temperature=0)
+
+    # Prompt
+    prompt = PromptTemplate(
+        template="""You are a grader assessing whether an answer is useful to resolve a question. \n 
+        Here is the answer:
+        \n ------- \n
+        {generation} 
+        \n ------- \n
+        Here is the question: {question}
+        Give a binary score 'yes' or 'no' to indicate whether the answer is useful to resolve a question. \n
+        Provide the binary score as a JSON with a single key 'score' and no preamble or explanation.""",
+        input_variables=["generation", "question"],
+    )
+
+    answer_grader = prompt | llm | JsonOutputParser()
+    answer_grader.invoke({"question": question,"generation": generation})
+
+
+    
