c944142e2c2d466d1dbb4ac3a6705386509d2527
jyeshtha1799 <114448454+jyeshtha1799@users.noreply.github.com>
2024-08-06 18:47:59 -0400

    functions

--- main.py
+++ main.py
@@ -199,3 +199,88 @@ for uploaded_file in uploaded_files:
     generation : str
     documents : List[str]
 
+    def retrieve(state):
+        """
+        Retrieve documents
+
+        Args:
+            state (dict): The current graph state
+
+        Returns:
+            state (dict): New key added to state, documents, that contains retrieved documents
+        """
+        st.write("---RETRIEVE---")
+        question = state["question"]
+
+        # Retrieval
+        documents = retriever.get_relevant_documents(question)
+        return {"documents": documents, "question": question}
+
+    def generate(state):
+        """
+        Generate answer
+
+        Args:
+            state (dict): The current graph state
+
+        Returns:
+            state (dict): New key added to state, generation, that contains LLM generation
+        """
+        st.write("---GENERATE---")
+        question = state["question"]
+        documents = state["documents"]
+        
+        # RAG generation
+        generation = rag_chain.invoke({"context": documents, "question": question})
+        return {"documents": documents, "question": question, "generation": generation}
+
+    def grade_documents(state):
+        """
+        Determines whether the retrieved documents are relevant to the question.
+
+        Args:
+            state (dict): The current graph state
+
+        Returns:
+            state (dict): Updates documents key with only filtered relevant documents
+        """
+
+        st.write("---CHECK DOCUMENT RELEVANCE TO QUESTION---")
+        question = state["question"]
+        documents = state["documents"]
+        
+        # Score each doc
+        filtered_docs = []
+        for d in documents:
+            score = retrieval_grader.invoke({"question": question, "document": d.page_content})
+            grade = score['score']
+            if grade == "yes":
+                st.write("---GRADE: DOCUMENT RELEVANT---")
+                filtered_docs.append(d)
+            else:
+                st.write("---GRADE: DOCUMENT NOT RELEVANT---")
+                continue
+        return {"documents": filtered_docs, "question": question}
+
+    def transform_query(state):
+        """
+    Transform the query to produce a better question.
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Updates question key with a re-phrased question
+    """
+
+        print("---TRANSFORM QUERY---")
+        question = state["question"]
+        documents = state["documents"]
+
+    # Re-write question
+        better_question = question_rewriter.invoke({"question": question})
+        return {"documents": documents, "question": better_question}
+
+    
+
+
