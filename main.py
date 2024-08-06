24a6dbcb1872a5c96ae0f013b4863a4278ef24b1
jyeshtha1799 <114448454+jyeshtha1799@users.noreply.github.com>
2024-08-06 16:45:56 -0400

    prompt2 for grading relevance of document about a user's question

--- main.py
+++ main.py
@@ -89,3 +89,21 @@ for uploaded_file in uploaded_files:
     llm = ChatOllama(model=local_llm, format="json", temperature=0)
 
     
+    prompt = PromptTemplate(
+            template="""You are a grader assessing relevance of a retrieved document to a user question. \n 
+            Here is the retrieved document: \n\n {document} \n\n
+            Here is the user question: {question} \n
+            If the document contains keywords related to the user question, grade it as relevant. \n
+            It does not need to be a stringent test. The goal is to filter out erroneous retrievals. \n
+            Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question. \n
+            Provide the binary score as a JSON with a single key 'score' and no premable or explaination.""",
+            input_variables=["question", "document"],
+        )
+
+    retrieval_grader = prompt | llm | JsonOutputParser()
+    question = "agent memory"
+    docs = retriever.get_relevant_documents(question)
+    doc_txt = docs[1].page_content
+    st.write(retrieval_grader.invoke({"question": question, "document": doc_txt}))
+
+    
