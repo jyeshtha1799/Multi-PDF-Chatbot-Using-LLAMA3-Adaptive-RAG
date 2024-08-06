76a16d2cfb2a8b8e9342af419aabc0b0a3af3118
jyeshtha1799 <114448454+jyeshtha1799@users.noreply.github.com>
2024-08-06 17:52:05 -0400

    scoring and  hallucinations

--- main.py
+++ main.py
@@ -124,4 +124,25 @@ for uploaded_file in uploaded_files:
     generation = rag_chain.invoke({"context": docs, "question": question})
     print(generation)
 
+
+    ### Hallucination Grader 
+    # LLM
+    llm = ChatOllama(model=local_llm, format="json", temperature=0)
+
+    # Prompt
+    prompt = PromptTemplate(
+        template="""You are a grader assessing whether an answer is grounded in / supported by a set of facts. \n 
+        Here are the facts:
+        \n ------- \n
+        {documents} 
+        \n ------- \n
+        Here is the answer: {generation}
+        Give a binary score 'yes' or 'no' score to indicate whether the answer is grounded in / supported by a set of facts. \n
+        Provide the binary score as a JSON with a single key 'score' and no preamble or explanation.""",
+        input_variables=["generation", "documents"],
+    )
+
+    hallucination_grader = prompt | llm | JsonOutputParser()
+    hallucination_grader.invoke({"documents": docs, "generation": generation})
+
     
